6e2321c21ed8f65aef1448322780ff9bd8709d08
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-23 16:53:07 -0500

    connect DB Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'inmueble-app';
+
+  constructor(private firebase: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.firebase.collection("test").stateChanges().subscribe(data => {
+      console.log(data.map(users => users.payload.doc.data()))
+    })
+
+  }
 }
