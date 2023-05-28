52278d36e73b834354b55fb0c436cdc059353b59
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-27 23:34:37 -0500

    add MatCardModule and style the component home

--- src/app/modules/pages/static/pages/home/home.component.ts
+++ src/app/modules/pages/static/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+  menulink: { accessLink: Array<any> } = { accessLink: [] }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.menulink.accessLink = [
+      {
+        name: 'Facebook',
+        icon: 'uil uil-facebook-f',
+        link: 'https://www.facebook.com/geovanny.alex.mr'
+      },
+      {
+        name: 'Github',
+        icon: 'uil uil-github',
+        link: 'https://github.com/GeovannyAlExRs'
+      },
+      {
+        name: 'Linkedin',
+        icon: 'uil uil-linkedin',
+        link: 'https://www.linkedin.com/in/geovanny-magallan-59ab5b99',
+      },
+      {
+        name: 'Geovanny',
+        icon: 'uil uil-link',
+        link: ['GeovannyAlexRs']
+      }
+    ]
+
+  }
 
 }
