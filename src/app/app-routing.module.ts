64a4df58dba8771c50dbf1dff3d24ad01f128b95
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-27 21:20:21 -0500

    add routes pages static of home and 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [{path: 'static', loadChildren: () => import('@modules/pages/static/static.module').then(m=>m.StaticModule)}]
+  },
+  { path: '', pathMatch:'full', redirectTo: 'static/home' },
+  { path: '**', pathMatch:'full', redirectTo: 'static/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/pages/static/static-routing.module.ts
+++ src/app/modules/pages/static/static-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', loadChildren: () => import('@modules/pages/static/pages/home/home.module').then(m => m.HomeModule)},
+  {path: '404', loadChildren: () => import('@modules/pages/static/pages/not-found/not-found.module').then(m => m.NotFoundModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
