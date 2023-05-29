74f4787328e827e200a0b066169c6036e3f07a49
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-29 12:54:12 -0500

    mockup and style component login

--- src/app/modules/pages/auth/pages/login/login.module.ts
+++ src/app/modules/pages/auth/pages/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -19,6 +20,7 @@ import { LoginComponent } from './login.component';
     CommonModule,
     LoginRoutingModule,
     MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     FormsModule,
     MatCardModule,
