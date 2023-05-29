dd96ab64609308cb009370379dfded048afb5f7e
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-29 14:53:33 -0500

    mockup and style component register

--- src/app/modules/pages/auth/pages/register/register.component.ts
+++ src/app/modules/pages/auth/pages/register/register.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  registerUser(form: NgForm) : void {
+
+  }
 }
