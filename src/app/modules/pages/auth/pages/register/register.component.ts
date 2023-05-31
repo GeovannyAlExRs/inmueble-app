0de98a3b4556890ddfa9d79b723b95c8017b1a1f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-31 18:42:06 -0500

    implement Sign Up User in method registerUser

--- src/app/modules/pages/auth/pages/register/register.component.ts
+++ src/app/modules/pages/auth/pages/register/register.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
 
+import { UserCreateRequest } from '@store/users/users.store.interfaces'
+import { getLoading } from '@store/users/users.store.selectors'
+import { SignUpEmail } from '@store/users/users.store.actions'
+import * as fromRoot from '@store/store'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,13 +14,26 @@ import { NgForm } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {}
+  logind$!: Observable<boolean | null>
+
+  constructor(private store: Store<fromRoot.State>) {}
 
   ngOnInit(): void {
-
+    this.logind$ = this.store.pipe(select(getLoading))
   }
 
   registerUser(form: NgForm) : void {
+    if(form.valid) {
+      const userCreateRequest: UserCreateRequest = {
+        firstname: form.value.firstname,
+        lastname: form.value.lastname,
+        username: form.value.username,
+        phone: form.value.phone,
+        email: form.value.email,
+        password: form.value.password
+      }
 
+      this.store.dispatch(new SignUpEmail(userCreateRequest))
+    }
   }
 }
