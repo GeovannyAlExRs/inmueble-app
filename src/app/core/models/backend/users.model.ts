b6e4cbd3a101267a3fc047b88e911d3893a86f7f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-31 18:20:05 -0500

    create interface Users Model

---
+++ src/app/core/models/backend/users.model.ts
@@ -0,0 +1,25 @@
+export interface Users {
+
+  id?: number | string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  phone: string;
+  email: string;
+  token?: string;
+
+  /*
+  firstName: string;
+  lastName: string;
+  userName: string;
+  phone: string;
+  email: string;
+  token: Token;
+   */
+}
+/*
+interface Token {
+  refresh: string;
+  access: string;
+}
+*/
