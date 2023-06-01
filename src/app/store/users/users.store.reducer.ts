195aa046a30348e632d2082da5f81bfaa1d2c548
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-01 12:32:39 -0500

    modific user store reducer

--- src/app/store/users/users.store.reducer.ts
+++ src/app/store/users/users.store.reducer.ts
@@ -22,24 +22,24 @@ export function reducer(state = initialState, action: fromActions.All | any) : U
   switch(action.type) {
     // INIT
     case fromActions.Types.INIT : return { ...state, loading: true }
-    case fromActions.Types.INIT_AUTHORIZED : return { ...state, loading: false, entity: action.user, id: action.id, error: null }
-    case fromActions.Types.INIT_ERROR : return { ...state, loading: false, entity: null, id: null, error: action.error }
-    case fromActions.Types.INIT_UNAUTHORIZED : return { ...state, loading: false, entity: null, id: null, error: null }
+    case fromActions.Types.INIT_AUTHORIZED : return { ...state, loading: false, entity: action.user, email: action.email, error: null }
+    case fromActions.Types.INIT_ERROR : return { ...state, loading: false, entity: null, email: null, error: action.error }
+    case fromActions.Types.INIT_UNAUTHORIZED : return { ...state, loading: false, entity: null, email: null, error: null }
 
     // LOGIN
-    case fromActions.Types.SIGN_IN_EMAIL : return { ...state, loading: true, entity: null, id: null, error: null }
-    case fromActions.Types.SIGN_IN_EMAIL_SUCCESS : return { ...state, loading: false, entity: action.user, id: action.id, error: null }
-    case fromActions.Types.SIGN_IN_EMAIL_ERROR : return { ...state, loading: false, entity: null, id: null, error: action.error }
+    case fromActions.Types.SIGN_IN_EMAIL : return { ...state, loading: true, entity: null, email: null, error: null }
+    case fromActions.Types.SIGN_IN_EMAIL_SUCCESS : return { ...state, loading: false, entity: action.user, email: action.email, error: null }
+    case fromActions.Types.SIGN_IN_EMAIL_ERROR : return { ...state, loading: false, entity: null, email: null, error: action.error }
 
     // REGISTER
-    case fromActions.Types.SIGN_UP_EMAIL : return { ...state, loading: true, entity: null, id: null, error: null }
-    case fromActions.Types.SIGN_UP_EMAIL_SUCCESS : return { ...state, loading: false, entity: action.user, id: action.id, error: null }
-    case fromActions.Types.SIGN_UP_EMAIL_ERROR : return { ...state, loading: false, entity: null, id: null, error: action.error }
+    case fromActions.Types.SIGN_UP_EMAIL : return { ...state, loading: true, entity: null, email: null, error: null }
+    case fromActions.Types.SIGN_UP_EMAIL_SUCCESS : return { ...state, loading: false, entity: action.user, email: action.email, error: null }
+    case fromActions.Types.SIGN_UP_EMAIL_ERROR : return { ...state, loading: false, entity: null, email: null, error: action.error }
 
     // LOGOUT
     case fromActions.Types.SIGN_OUT_EMAIL : return { ...initialState }
     case fromActions.Types.SIGN_OUT_EMAIL_SUCCESS : return { ...initialState }
-    case fromActions.Types.SIGN_OUT_EMAIL_ERROR : return { ...state, loading: false, entity: null, id: null, error: action.error }
+    case fromActions.Types.SIGN_OUT_EMAIL_ERROR : return { ...state, loading: false, entity: null, email: null, error: action.error }
 
     default: { return state }
   }
