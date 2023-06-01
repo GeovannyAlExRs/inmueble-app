773ccd73ba18fa05cbe839408e369d7b6a9d185f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-01 17:14:03 -0500

    restrict menu in the parent  component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { NotificationService } from './services/notification/notification.service';
+
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import * as fromRoot from '@store/store'
+import { getUser, getIsAuthorized } from '@store/users/users.store.selectors'
+import { UsersResponse } from '@store/users/users.store.interfaces'
+import { Init ,SignOutEmail } from '@store/users/users.store.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +21,22 @@ export class AppComponent implements OnInit {
   showSpinner = false;
   title = 'inmueble-app';
 
-  constructor(private firebase: AngularFirestore, private notificationService: NotificationService) {}
+  users$!: Observable<UsersResponse>
+  isAuthorized$!: Observable<boolean>
+
+  constructor(private firebase: AngularFirestore, private notificationService: NotificationService,
+              private store: Store<fromRoot.State>, private router: Router) {}
 
   ngOnInit(): void {
-    this.firebase.collection("test").stateChanges().subscribe(data => {
+    /*this.firebase.collection("test").stateChanges().subscribe(data => {
       console.log(data.map(users => users.payload.doc.data()))
-    })
+    })*/
+    this.users$ = this.store.pipe(select(getUser)) as Observable<UsersResponse>
+    this.isAuthorized$ = this.store.pipe(select(getIsAuthorized)) as Observable<boolean>
 
+    console.log('USER: ', this.users$);
+
+    //this.store.dispatch(new Init())
   }
 
   onToggleSpinner() : void {
@@ -34,5 +53,12 @@ export class AppComponent implements OnInit {
 
   onError() : void {
     this.notificationService.error("El procedimiento fue INCORRECTO")
+  }
+
+  logoutUser(): void {
+    //console.log('main app: ', main);
+    localStorage.removeItem('token')
+    this.store.dispatch(new SignOutEmail())
+    this.router.navigate(['/auth/login'])
   }
 }
