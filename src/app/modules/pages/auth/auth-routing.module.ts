5a6cda5f8fabb413ae3a4a35f02b5f910baed3c9
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-01 21:06:22 -0500

    implement UnauthGuard in path login and register

--- src/app/modules/pages/auth/auth-routing.module.ts
+++ src/app/modules/pages/auth/auth-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnauthGuard } from '@core/guards/unauth/unauth.guard';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('@modules/pages/auth/pages/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('@modules/pages/auth/pages/register/register.module').then(m => m.RegisterModule) },
+  { path: 'login', loadChildren: () => import('@modules/pages/auth/pages/login/login.module').then(m => m.LoginModule),
+    canActivate: [UnauthGuard] },
+  { path: 'register', loadChildren: () => import('@modules/pages/auth/pages/register/register.module').then(m => m.RegisterModule),
+    canActivate: [UnauthGuard] },
   { path: '**', pathMatch: 'full', redirectTo: 'login' }
 ];
 
