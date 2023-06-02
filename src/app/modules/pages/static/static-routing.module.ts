8d2882a07c03cfbd07a47efc8facd1f1669d29ac
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-01 21:05:35 -0500

    implement AuthGuard in the paht home

--- src/app/modules/pages/static/static-routing.module.ts
+++ src/app/modules/pages/static/static-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth/auth.guard';
 
 const routes: Routes = [
-  {path: 'home', loadChildren: () => import('@modules/pages/static/pages/home/home.module').then(m => m.HomeModule)},
-  {path: '404', loadChildren: () => import('@modules/pages/static/pages/not-found/not-found.module').then(m => m.NotFoundModule)},
+  { path: 'home', loadChildren: () => import('@modules/pages/static/pages/home/home.module').then(m => m.HomeModule),
+    canActivate: [AuthGuard] },
+  { path: '404', loadChildren: () => import('@modules/pages/static/pages/not-found/not-found.module').then(m => m.NotFoundModule)},
 ];
 
 @NgModule({
