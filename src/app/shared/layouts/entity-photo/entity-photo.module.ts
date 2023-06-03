3b3bba13ea61e1c2e42308ad1fec00854617c6d9
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 18:54:04 -0500

    Create module of entity photo

---
+++ src/app/shared/layouts/entity-photo/entity-photo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EntityPhotoComponent } from './entity-photo.component';
+
+
+
+@NgModule({
+  declarations: [
+    EntityPhotoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EntityPhotoModule { }
