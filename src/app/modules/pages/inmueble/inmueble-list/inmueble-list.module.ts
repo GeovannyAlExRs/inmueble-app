6057be730b684de2fb25351233b4a714d45ff656
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 13:39:25 -0500

    create module and route of inmueble list

---
+++ src/app/modules/pages/inmueble/inmueble-list/inmueble-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InmuebleListRoutingModule } from './inmueble-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InmuebleListRoutingModule
+  ]
+})
+export class InmuebleListModule { }
