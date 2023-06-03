ceb5188f3d25dde84d074c10c9b27ada21f23792
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 13:44:39 -0500

    add path of inmueble form and list

--- src/app/modules/pages/inmueble/inmueble-form/inmueble-form-routing.module.ts
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InmuebleFormComponent } from './inmueble-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InmuebleFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/pages/inmueble/inmueble-list/inmueble-list-routing.module.ts
+++ src/app/modules/pages/inmueble/inmueble-list/inmueble-list-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InmuebleListComponent } from './inmueble-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InmuebleListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
