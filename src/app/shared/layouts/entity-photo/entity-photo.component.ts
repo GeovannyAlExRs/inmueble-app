9c2698cf48510ba457e1630d70708c248ac97a2f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 18:55:15 -0500

    Create component entity photo

---
+++ src/app/shared/layouts/entity-photo/entity-photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entity-photo',
+  templateUrl: './entity-photo.component.html',
+  styleUrls: ['./entity-photo.component.scss']
+})
+export class EntityPhotoComponent {
+
+}
