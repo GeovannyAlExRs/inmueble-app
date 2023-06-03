0af6e85c32467a6f5218b82251622b48f3f4ee92
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 13:41:12 -0500

    create component of inmueble list

--- src/app/modules/pages/inmueble/inmueble-list/inmueble-list.module.ts
+++ src/app/modules/pages/inmueble/inmueble-list/inmueble-list.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InmuebleListRoutingModule } from './inmueble-list-routing.module';
+import { InmuebleListComponent } from './inmueble-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InmuebleListComponent
+  ],
   imports: [
     CommonModule,
     InmuebleListRoutingModule
