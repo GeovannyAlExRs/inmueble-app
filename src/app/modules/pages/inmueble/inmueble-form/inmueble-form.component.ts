e68e061d2a59e43016fd4338a547dac231368301
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 13:42:10 -0500

    create component of inmueble form

---
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inmueble-form',
+  templateUrl: './inmueble-form.component.html',
+  styleUrls: ['./inmueble-form.component.scss']
+})
+export class InmuebleFormComponent {
+
+}
--- src/app/modules/pages/inmueble/inmueble-form/inmueble-form.module.ts
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InmuebleFormRoutingModule } from './inmueble-form-routing.module';
+import { InmuebleFormComponent } from './inmueble-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InmuebleFormComponent
+  ],
   imports: [
     CommonModule,
     InmuebleFormRoutingModule
