f9c394aa6296ed102e41e2f6632f6faa976bd26f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 19:14:23 -0500

    exports module entity photo layout

--- src/app/shared/layouts/layouts.module.ts
+++ src/app/shared/layouts/layouts.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EntityPhotoModule } from './entity-photo/entity-photo.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    EntityPhotoModule
+  ],
+  exports: [
+    EntityPhotoModule
   ]
 })
 export class LayoutsModule { }
