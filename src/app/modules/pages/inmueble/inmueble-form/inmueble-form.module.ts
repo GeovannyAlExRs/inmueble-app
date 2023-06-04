c47b0d73448c0612fda036a21287ce8a7bba0814
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-03 21:05:33 -0500

    import module in inmueble form

--- src/app/modules/pages/inmueble/inmueble-form/inmueble-form.module.ts
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { InmuebleFormRoutingModule } from './inmueble-form-routing.module';
 import { InmuebleFormComponent } from './inmueble-form.component';
+
+import { IndicatorsModule } from '@shared/indicators/indicators.module';
+import { PopusModule } from '@shared/popus/popus.module';
+import { EntityPhotoModule } from '@shared/layouts/entity-photo/entity-photo.module';
+
 
 
 @NgModule({
@@ -11,7 +26,18 @@ import { InmuebleFormComponent } from './inmueble-form.component';
   ],
   imports: [
     CommonModule,
-    InmuebleFormRoutingModule
+    InmuebleFormRoutingModule,
+
+    FormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    IndicatorsModule,
+    PopusModule,
+    EntityPhotoModule,
   ]
 })
 export class InmuebleFormModule { }
