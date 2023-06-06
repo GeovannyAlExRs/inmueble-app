8b4987102fb0b18239c95c4fb37b075d411c5a51
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-06 15:28:34 -0500

    Implement upload file img app-entity-photo

--- src/app/modules/pages/inmueble/inmueble-form/inmueble-form.component.ts
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inmueble-form',
   templateUrl: './inmueble-form.component.html',
   styleUrls: ['./inmueble-form.component.scss']
 })
-export class InmuebleFormComponent {
+export class InmuebleFormComponent implements OnInit {
 
+  loading$!: Observable<boolean | null>
+  photoLoaded!: string
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  saveInmueble(form: NgForm) : void {
+
+  }
+
+  onFilesImg(url: any) : void {
+    if(url) {
+      this.photoLoaded = url
+      //console.log('URL', this.photoLoaded);
+    }
+  }
 }
