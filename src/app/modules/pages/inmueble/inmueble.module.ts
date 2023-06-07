dfbb0137cdc462357e51edb5ee31516bd58afb3c
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-06 22:12:56 -0500

    import module reducers and effects of inmueble

--- src/app/modules/pages/inmueble/inmueble.module.ts
+++ src/app/modules/pages/inmueble/inmueble.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InmuebleRoutingModule } from './inmueble-routing.module';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { effects, reducers } from './store/inmueble.store';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    InmuebleRoutingModule
+    InmuebleRoutingModule,
+
+    StoreModule.forFeature('inmueble', reducers),
+    EffectsModule.forFeature(effects)
   ]
 })
 export class InmuebleModule { }
