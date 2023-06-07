fc9ec17aff0fcafbb8ece1cd7a1f781c0e0a5c19
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-06 22:29:03 -0500

    create method saveInmueble

--- src/app/modules/pages/inmueble/inmueble-form/inmueble-form.component.ts
+++ src/app/modules/pages/inmueble/inmueble-form/inmueble-form.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
+import { Create } from '@modules/pages/inmueble/store/inmueble/inmueble.store.actions'
+import { InmuebleCreateRequest } from '@modules/pages/inmueble/store/inmueble/inmueble.store.interfaces'
+import { getLoading } from '@modules/pages/inmueble/store/inmueble/inmueble.store.selectors'
+import * as fromRoot from '@modules/pages/inmueble/store/inmueble.store'
 
 @Component({
   selector: 'app-inmueble-form',
@@ -12,12 +19,23 @@ export class InmuebleFormComponent implements OnInit {
   loading$!: Observable<boolean | null>
   photoLoaded!: string
 
-  constructor() {}
+  constructor(private store: Store<fromRoot.InmuebleListState>) {}
 
   ngOnInit(): void {}
 
   saveInmueble(form: NgForm) : void {
+    if(form.valid) {
+      this.loading$ = this.store.pipe(select(getLoading))
 
+      const inmuebleCreateRequest : InmuebleCreateRequest = {
+        name: form.value.name,
+        address: form.value.address,
+        price: Number(form.value.price),
+        photo: this.photoLoaded
+      }
+
+      this.store.dispatch(new Create(inmuebleCreateRequest))
+    }
   }
 
   onFilesImg(url: any) : void {
