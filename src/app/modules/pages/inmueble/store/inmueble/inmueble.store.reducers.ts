4176abe8f8215c2663450ba687dfcf8746982fea
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-29 15:38:51 -0500

    add status for read inmueble

--- src/app/modules/pages/inmueble/store/inmueble/inmueble.store.reducers.ts
+++ src/app/modules/pages/inmueble/store/inmueble/inmueble.store.reducers.ts
@@ -2,12 +2,14 @@ import { InmuebleResponse } from "./inmueble.store.interfaces";
 import * as fromActions from "./inmueble.store.actions";
 
 export interface InmuebleState {
+  inmuebles: InmuebleResponse[] | null;
   inmueble: InmuebleResponse | null;
   loading: boolean | null;
   error: string | null;
 }
 
 const initialState: InmuebleState = {
+  inmuebles: null,
   inmueble: null,
   loading: null,
   error: null
@@ -18,6 +20,11 @@ export function reducer(state: InmuebleState = initialState, action: fromActions
     case fromActions.Types.CREATE: return {...state, loading: true, error: null }
     case fromActions.Types.CREATE_SUCCESS: return {...state, inmueble: action.inmueble, loading: false, error: null }
     case fromActions.Types.CREATE_ERROR: return {...state, loading: false, error: action.error }
+
+    case fromActions.Types.READ: return {...state, loading: true, error: null }
+    case fromActions.Types.READ_SUCCESS: return {...state, loading: false, inmuebles: action.inmuebles }
+    case fromActions.Types.READ_ERROR: return {...state, loading: false, error: action.error }
+
     default: return state
   }
 }
