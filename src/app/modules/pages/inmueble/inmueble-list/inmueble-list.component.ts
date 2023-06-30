366fa6db23e9c8df4b66b41f4b34bf6eba7e9d32
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-06-30 15:18:07 -0500

    create compoent list inmueble

--- src/app/modules/pages/inmueble/inmueble-list/inmueble-list.component.ts
+++ src/app/modules/pages/inmueble/inmueble-list/inmueble-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Read } from '../store/inmueble/inmueble.store.actions';
+import { InmuebleResponse } from '../store/inmueble/inmueble.store.interfaces';
+import * as fromRoot from '@store/store'
+import { getInmuebles, getLoading } from '../store/inmueble/inmueble.store.selectors';
 
 @Component({
   selector: 'app-inmueble-list',
   templateUrl: './inmueble-list.component.html',
   styleUrls: ['./inmueble-list.component.scss']
 })
-export class InmuebleListComponent {
+export class InmuebleListComponent implements OnInit {
 
+  //listBtn: { btn: Array<any> } = { btn: [] }
+
+  loading$!: Observable<boolean | null>
+  inmuebles$!: Observable<InmuebleResponse[] | null>
+
+  searchInmueble: string = ''
+
+  constructor(private store: Store<fromRoot.State>) {}
+
+  ngOnInit(): void {
+
+    this.store.dispatch(new Read())
+    this.loading$ = this.store.pipe(select(getLoading))
+    this.inmuebles$ = this.store.pipe(select(getInmuebles))
+    console.log('INMUEBLES: ', this.inmuebles$);
+
+  }
+
+  receiveData(event: string): void {
+    console.log('Estoy desde el padre: ', event);
+    this.searchInmueble = event
+  }
 }
--- src/app/modules/pages/inmueble/inmueble-list/inmueble-list.module.ts
+++ src/app/modules/pages/inmueble/inmueble-list/inmueble-list.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { InmuebleListRoutingModule } from './inmueble-list-routing.module';
 import { InmuebleListComponent } from './inmueble-list.component';
+import { IndicatorsModule } from '@shared/indicators/indicators.module';
+import { ImgBrokenModule } from '@shared/directives/img-broken.module';
+import { SearchModule } from '@shared/pipe/search.module';
+import { LayoutsModule } from '@shared/layouts/layouts.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -11,7 +21,17 @@ import { InmuebleListComponent } from './inmueble-list.component';
   ],
   imports: [
     CommonModule,
-    InmuebleListRoutingModule
+    InmuebleListRoutingModule,
+
+    FlexLayoutModule,
+    IndicatorsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+
+    ImgBrokenModule,
+    SearchModule,
+    LayoutsModule
   ]
 })
 export class InmuebleListModule { }
